40467fb1dadb385f4e928c34df22c82ca85675a1
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-26 19:56:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,7 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-      @brand = brand
-  #     if BRANDS.include?(@brand)
-  #   else 
-  #       BRANDS << @brand 
-  # end
+      @brand = brand 
    
         BRANDS.push(@brand) unless BRANDS.include?(@brand)
    end
